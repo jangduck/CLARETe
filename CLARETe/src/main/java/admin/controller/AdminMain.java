6bc371da5fe1d9913fbc6906b642437d5cc40109
Kang-Yi-Hun <167230462+Kang-Yi-Hun@users.noreply.github.com>
2024-12-22 17:47:35 +0900

    계속 충돌나던 Admin.Main.java 삭제했던 거 파일 다시 생성 후 푸쉬

---
+++ CLARETe/src/main/java/admin/controller/AdminMain.java
@@ -0,0 +1,39 @@
+package admin.controller;
+
+import common.controller.AbstractController;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import member.domain.MemberVO;
+
+public class AdminMain extends AbstractController {
+
+	@Override
+	public void execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		
+		HttpSession session = request.getSession();
+		
+		MemberVO loginuser = (MemberVO) session.getAttribute("loginuser");
+		
+		if( loginuser != null && "admin".equals(loginuser.getM_id()) ) {
+			// 관리자로 로그인 한 경우
+			
+			super.setRedirect(false);
+			super.setViewPage("/WEB-INF/admin/adminMain.jsp");
+
+		}
+		else {
+			// 로그인을 안한 경우 또는 일반사용자로 로그인 한 경우
+			String message = "관리자만 접근이 가능합니다.";
+			String loc = "javascript:history.back()";
+			
+			request.setAttribute("message", message);
+			request.setAttribute("loc", loc);
+			
+			super.setRedirect(false);
+			super.setViewPage("/WEB-INF/msg.jsp");
+		}
+		
+	}
+
+}
